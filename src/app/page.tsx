8d226bacee16c5f932a2aecb105dfa49629b3047
Alex <alexpleonhardt@gmail.com>
2024-10-30 16:22:08 -0400

    fix: semantic html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
         <div className="h-[40rem] max-w-6xl m-auto px-8 flex justify-center items-center md:justify-end">
           <div className="font-bold bg-gradient-to-r from-black to-transparent p-4 max-w-[28rem] flex flex-col gap-4">
-            <p className="text-white text-3xl">Give Your Pet the Pampering They Deserve!</p>
+            <h1 className="text-white text-3xl">Give Your Pet the Pampering They Deserve!</h1>
             <div>
               <button className="bg-green-700 text-xl text-white rounded-md p-2 hover:bg-green-900">Book Appointment</button>
             </div>
