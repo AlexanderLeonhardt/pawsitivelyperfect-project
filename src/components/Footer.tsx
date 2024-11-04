c0d097b0433c543e0d1bd97f2392273fafa020f1
Alex <alexpleonhardt@gmail.com>
2024-11-04 11:53:50 -0500

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-neutral-950 text-neutral-300 flex flex-col gap-4 p-8 items-center">
+      <div className="text-center">
+        <p>1234 Paws Avenue, Dogcity, FL 12345</p>
+        <p>(555) 123-4567</p>
+        <p><a href="mailto:info@pawsitivelyperfectgrooming.com">info@pawsitivelyperfectgrooming.com</a></p>
+      </div>
+
+      <p className="text-neutral-600">Copyright © 2024 Pawsitively Perfect Grooming LLC. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
