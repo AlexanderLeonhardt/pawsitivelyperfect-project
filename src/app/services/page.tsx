f4e6cbc2f416f88a53a16cdcb756fdcc5a672b82
Alex <alexpleonhardt@gmail.com>
2024-11-05 15:20:14 -0500

    specialty add-on services

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -13,6 +13,30 @@ const ServicesPage = () => {
           </div>
         </div>
       </section>
+
+      <section className="my-12">
+        <div className="max-w-6xl m-auto px-8 flex flex-col items-center gap-4">
+          <h2 className="text-3xl font-bold text-center">Specialty Add-Ons</h2>
+          <hr className="w-32 border-t-2 border-green-700"/>
+          <div className="text-lg flex flex-col items-start gap-4">
+            <p>Choose one or more available add-ons for your pet&apos;s visit.</p>
+            <ul>
+              <li>
+                <h3>Flea & Tick Treatment <span>$10</span></h3>
+                <p>Keep pests at bay with our specialized flea and tick shampoo treatment, designed to provide lasting protection.</p>
+              </li>
+              <li>
+                <h3>Ear Cleaning & Deodorizing <span>$7</span></h3>
+                <p>A gentle ear cleaning service that removes wax buildup and leaves your pet&apos;s ears smelling fresh.</p>
+              </li>
+              <li>
+                <h3>Custom Fur Styling <span>$20</span></h3>
+                <p>Looking for a unique style? Our groomers offer custom fur cuts tailored to your preference—whether it&apos;s a fun trim or a breed-specific look.</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
