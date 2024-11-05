a42376db443b6131f929b8314d6d1ef0449c39fd
Alex <alexpleonhardt@gmail.com>
2024-11-05 14:04:34 -0500

    services overview section

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -2,7 +2,13 @@
 const ServicesPage = () => {
   return (
     <main>
-      <h1>Services Page</h1>
+      <section className="my-12">
+        <div className="max-w-6xl m-auto px-8 flex flex-col items-center gap-4">
+          <div className="text-lg flex flex-col items-start gap-4">
+            <p>At Pawsitively Perfect Grooming, we offer a full range of grooming services tailored to meet the specific needs of your pet. From baths and brushes to specialized grooming packages, our services are designed to make your pet look and feel their best. Explore our service options below to find the perfect fit for your furry friend.</p>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
