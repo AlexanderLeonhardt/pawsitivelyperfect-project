dc896efd689e9fbfc969187d9f8edac66432b048
Alex <alexpleonhardt@gmail.com>
2024-11-08 18:50:38 -0500

    pichdate component

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,10 @@
+import PickDate from "@/components/PickDate";
 
 const ContactPage = () => {
   return (
     <main>
       <h1>Contact Page</h1>
+      <PickDate />
     </main>
   );
 }
---
+++ src/components/PickDate.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { useState } from "react";
+import { getDay, subDays } from "date-fns";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const PickDate = () => {
+  const [startDate, setStartDate] = useState<Date | null>(null);
+
+  const excludeDates = (date: Date) => {
+    const day = getDay(date);
+    return day !== 0 && date > subDays(new Date(), 1);
+  };
+
+  return (
+    <DatePicker
+      selected={startDate}
+      onChange={(date) => setStartDate(date)}
+      filterDate={(date) => excludeDates(date)}
+      placeholderText="Select a date"
+    />
+  );
+}
+
+export default PickDate;
