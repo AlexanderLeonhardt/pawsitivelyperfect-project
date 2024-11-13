bc9b99ffa48ec86645e942b722d49597c29e3980
Alex <alexpleonhardt@gmail.com>
2024-11-13 09:15:16 -0500

    book appointment section

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,9 +3,15 @@ import ContactForm from "@/components/ContactForm";
 const ContactPage = () => {
   return (
     <main>
-      <div className="mx-auto p-4 md:w-1/2">
-        <ContactForm />
-      </div>
+      <section className="my-12">
+        <div className="max-w-6xl m-auto px-8 flex flex-col items-center gap-4">
+          <h2 className="text-3xl font-bold text-center">Book an Appointment</h2>
+          <hr className="w-32 border-t-2 border-green-700"/>
+          <div className="text-lg flex flex-col items-start gap-4">
+            <ContactForm />
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
